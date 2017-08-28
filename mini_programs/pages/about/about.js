6a75c235a25d18fef646548c9adae83150f3b08a
urinx <1336006643@qq.com>
2017-08-28 20:28:15 +0800

    add user talk service

--- mini_programs/pages/about/about.js
+++ mini_programs/pages/about/about.js
@@ -18,14 +18,14 @@ Page({
 
     feedback_btn: function(){
     	wx.showActionSheet({
-		  itemList: ['你可以通过以下方式反馈问题', '邮箱: ai@urinx.me', '公众号: Urinx'],
+		  itemList: ['你可以通过以下方式反馈问题', '邮箱: ai@urinx.me', '公众号: Urinx', '客服会话中发送 人工客服 关键字'],
 		})
     },
 
     update_btn: function(){
     	wx.showModal({
 		  title: '更新日志',
-		  content: '2017.8.28\n1. 优化分享和保存功能\n2. 修复bug\n3. 新增关于tab页面'
+		  content: '2017.8.28\n1. 优化分享和保存功能\n2. 修复bug\n3. 新增关于tab页面\n4. 增加客服会话功能'
 		})
     }
 })
